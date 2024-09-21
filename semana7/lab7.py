85799986b2a89b58ec55f890820cd866ab33e71c
Farce99 FAARCE <fabian.arce@uniminuto.edu.co>
2024-09-21 01:15:52 +0000

    primera practica del lab7

---
+++ semana7/lab7.py
@@ -0,0 +1,31 @@
+# student = {
+#    "name": "Fabian",
+#    "age":24
+# }
+
+# print(student["name"])
+
+def class_person(name,lastname,id,age,role):
+    return{
+        "name": name,
+        "lastname"; lastname,
+        "id": id,
+        "age":age,
+        "role":role
+    }
+
+def show_objet(object_)
+    print(object_)
+
+def add_person():
+    name = input("por favor digite su nombre: \n")
+    lastname = input("por favor digite su apellido: \n")
+    id = int(input("por favor digite su ID: \n"))
+    age = int(input("por favor digite su edad: \n"))
+    role = int(input("por favor digite su rol: \n"))
+
+add_person()
+
+# agregar personas a una lista
+# usar un bucle para agregar usuarios hasta el usuario decida no agregar mas
+# imprimir el contenido del listado
